9f6372be166be9927e6c40c4952a565bbb8a4865
Nataliia Rigoni <nataliiarigoni@gmail.com>
2024-03-18 13:50:47 -0500

    changed output file

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,15 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.project.id
-}
-output "public_subnet1" {
-  value = aws_subnet.public_subnet1.id
-}
-output "public_subnet2" {
-  value = aws_subnet.public_subnet2.id
-}
-output "public_subnet3" {
-  value = aws_subnet.public_subnet3.id
-}
 output "database_endpoint" {
   value = aws_rds_cluster.db_instance.endpoint
 }
