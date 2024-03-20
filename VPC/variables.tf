07f995c03270d97683f4b1b6023100838ef06bcd
Nataliia Rigoni <nataliiarigoni@gmail.com>
2024-03-20 17:17:56 -0500

    variables changed

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -5,6 +5,13 @@ variable "path_to_public_key" {
 
 }
 
+variable "path_to_private_key" {
+  description = "public key location on terraform instance"
+  type        = string
+  default     = "/home/ec2-user/.ssh/id_rsa"
+
+}
+
 variable "sec_group_name" {
   description = "please provide a sec_group_name information"
   type        = string
